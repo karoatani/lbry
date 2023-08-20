da25344def5b51bd28cb106e7f5667bbe5295a68
karoatani <karoatani@gmail.com>
2023-08-19 22:50:43 -0700

    Updated Crf Trusted Origin

--- lbry/settings/prod.py
+++ lbry/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://*.lbry-production.up.railway.app/','https://lbry-production.up.railway.app/*']
 
 DATABASES = {
     'default': dj_database_url.config(),
